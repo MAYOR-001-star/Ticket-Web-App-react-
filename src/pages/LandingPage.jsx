8fbb89144099ecd0824a9b5fddd486dc502dd1ac
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-10-27 17:51:38 +0100

    small touches

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,11 +5,11 @@ export default function LandingPage() {
   return (
     <section className="relative overflow-hidden bg-gradient-to-b from-blue-50 to-white min-h-[80vh] flex flex-col justify-center items-center text-center px-6">
       {/* Decorative Circles */}
-      <div className="absolute top-10 left-10 w-20 h-20 bg-blue-200 rounded-full blur-2xl opacity-70"></div>
-      <div className="absolute bottom-20 right-20 w-32 h-32 bg-blue-300 rounded-full blur-2xl opacity-50"></div>
+      <div class="absolute top-10 left-10 w-20 h-20 bg-blue-200 rounded-full blur-2xl opacity-70"></div>
+      <div class="absolute bottom-44 right-20 w-32 h-32 bg-blue-300 rounded-full blur-2xl opacity-50"></div>
 
       {/* Hero Content */}
-      <div className="max-w-[800px] z-10">
+      <div className="max-w-[800px] z-10 py-16">
         <h1 className="text-5xl font-bold text-gray-800 mb-4">
           Manage Your Tickets Seamlessly
         </h1>
@@ -34,11 +34,11 @@ export default function LandingPage() {
       </div>
 
       {/* Hero Wave */}
-      {/* <img
-        src={wave}
+      <img
+        src="/wave.svg"
         alt="wave background"
-        className="absolute bottom-0 left-0 w-full"
-      /> */}
+        className="mt-0"
+      />
     </section>
   );
 }
